28d3a0221ff900768d328e1a7c6043f3ed95ee25
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2023-07-04 17:32:47 -0300

    implementação dos imputs falta terminar e implementar os validadores dos inputs

--- formulario_frontend/lib/main.dart
+++ formulario_frontend/lib/main.dart
@@ -1,125 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:datetime_picker_formfield_new/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Formulario',
+      darkTheme: ThemeData(
+          brightness: Brightness.dark, primarySwatch: Colors.red
+          ),
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  final _Formkey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _Turmacontroller = TextEditingController();
+  final TextEditingController _Turnocontroller = TextEditingController();
+  final TextEditingController _Datacontroller = TextEditingController();
+  final TextEditingController _Datafinalcontroller = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Formulario Turma"),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _Formkey,
+              child: Column(children: [
+
+                //TURMA
+                TextFormField(
+                  controller: _Turmacontroller,
+                  decoration: const InputDecoration(
+                    labelText: "Turma: "
+                  ),
+                ),
+
+                //TURNO
+                TextFormField(
+                  controller: _Turnocontroller,
+                  decoration: const InputDecoration(
+                    labelText: "Turno: ",
+                  ),
+                ),
+
+                //DATA DE INICIO DO CURSO
+                DateTimeField(
+                controller: _Datacontroller,
+                decoration: InputDecoration(
+                    labelText: "Inicio do Curso: ",
+                    filled: true,
+                    prefixIcon: Icon(Icons.calendar_today),
+                  ),
+                format: DateFormat("dd/MM/yyyy"),
+                onShowPicker: (context, currentValue) {
+                  return showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(2100),
+                  );
+                },
+              ),
+
+                //DATA FINAL DO CURSO
+                DateTimeField(
+                controller: _Datafinalcontroller,
+                decoration: InputDecoration(
+                    labelText: "Inicio do Curso: ",
+                    filled: true,
+                    prefixIcon: Icon(Icons.calendar_today),
+                  ),
+                format: DateFormat("dd/MM/yyyy"),
+                onShowPicker: (context, currentValue) {
+                  return showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(2100),
+                  );
+                },
+                validator: (value) {
+                  if(value == null) {
+                    return "Campo Obrigatorio";
+                  }
+                },
+              ),
+                //HORAS DA AULA DO DIA
+                TextFormField()
+                //CURSO
+                //TextFormField()
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            )
+          ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+//     if(_picked != null){
+//       setState(() {
+//         _Datacontroller.text = _picked.toString().split("")[0];
+//       });
+//     }
+//   }
+// }
